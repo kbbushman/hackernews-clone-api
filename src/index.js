aecc011ee40d6f306e9957b50578d3261a7e31a4
kbbushman <k.bushman@yahoo.com>
2021-03-10 23:13:32 -0800

    Adds prisma client instance as context to apollo server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client')
 const resolvers = require('./resolvers');
+
+const prisma = new PrismaClient();
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(
@@ -9,9 +12,11 @@ const server = new ApolloServer({
     'utf8'
   ),
   resolvers,
+  context: {
+    prisma,
+  }
 });
 
-server.listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+server
+  .listen()
+  .then(({ url }) => console.log(`Server is running on ${url}`));
