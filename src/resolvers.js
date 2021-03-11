657191d62ffec6d33c476029c51850265b322a2d
kbbushman <k.bushman@yahoo.com>
2021-03-10 22:08:38 -0800

    Adds post link mutation resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,6 +4,8 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }];
 
+let idCount = links.length;
+
 module.exports = {
   Query: {
     info: () => 'This is the Hackernews Clone API',
@@ -11,5 +13,16 @@ module.exports = {
     link: (parent, args) => {
       return links.find((link) => link.id === args.id);
     }
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
