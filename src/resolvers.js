7a2fe911dfbd37b4461802fd383b976b110799bb
kbbushman <k.bushman@yahoo.com>
2021-03-10 22:03:29 -0800

    Adds links temp data and feed resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,12 @@
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}];
+
 module.exports = {
   Query: {
     info: () => 'This is the Hackernews Clone API',
+    feed: () => links,
   },
 };
